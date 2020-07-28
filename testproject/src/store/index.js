e9018f2b0aa1a0616055e9d32c1b5913d5f5bb8b
whiledu2 <trium10@gmail.com>
2020-07-28 15:46:49 +0900

    vuex 작업

--- testproject/src/store/index.js
+++ testproject/src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import items from "@/assets/items.json";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    items: null,
   },
+  // 동기 통신을 해야하는 곳
   mutations: {
+    getItems(state, payload) {
+      state.items = payload;
+    },
   },
+  // 비 동기 통신을 해야하는 곳
   actions: {
+    getItems ({commit}) {
+      let payload = items;
+
+      commit("getItems", payload);
+    },
   },
   modules: {
   }
---
+++ testproject/src/views/List.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card
+                        max-width="1000"
+                        class="mx-auto"
+                >
+                    <v-toolbar
+                            color="indigo"
+                            dark
+                    >
+                        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+                        <v-toolbar-title>Inbox</v-toolbar-title>
+
+                        <v-spacer></v-spacer>
+
+                        <v-btn icon>
+                            <v-icon>mdi-magnify</v-icon>
+                        </v-btn>
+
+                        <v-btn icon>
+                            <v-icon>mdi-dots-vertical</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                    <v-list>
+                        <v-list-item
+                                v-for="item in items"
+                                :key="item.title"
+                                @click=""
+                        >
+                            <v-list-item-icon>
+                                <v-icon v-if="item.icon" color="pink">mdi-star</v-icon>
+                            </v-list-item-icon>
+
+                            <v-list-item-content>
+                                <v-list-item-title v-text="item.title"></v-list-item-title>
+                            </v-list-item-content>
+
+                            <v-list-item-avatar>
+                                <v-img :src="item.avatar"></v-img>
+                            </v-list-item-avatar>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                items : null,
+            }
+        },
+        created() {
+            this.items = this.$store.state.items;
+        }
+    }
+</script>
